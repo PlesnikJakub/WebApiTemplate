1e15d7193a88493062df04b09d2dd43bc33277c9
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-08 22:22:18 +0100

    Remove usings

--- src/WebApi/WebApi/Modules/PersistenceExtension.cs
+++ src/WebApi/WebApi/Modules/PersistenceExtension.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApi.Modules
+﻿namespace WebApi.Modules
 {
     public class PersistenceExtension
     {
