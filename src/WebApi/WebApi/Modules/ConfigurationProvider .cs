8a7eb3115f0a4b1df37912bfb2eee60bf9169cbe
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-20 19:30:10 +0100

    Add configuration provider

---
+++ src/WebApi/WebApi/Modules/ConfigurationProvider .cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Modules
+{
+    public class ConfigurationProvider : Application.Providers.IConfigurationProvider
+    {
+        private readonly IConfiguration Configuration;
+
+        public ConfigurationProvider(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public string GetConfiguration(string key)
+        {
+            return Configuration.GetValue<string>(key);
+        }
+    }
+}
