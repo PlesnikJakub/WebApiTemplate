2b9706a43b8e6099127c862185a479be1e1e49e3
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-20 19:17:16 +0100

    Add context and entity

---
+++ src/WebApi/Infrastructure/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ src/WebApi/Infrastructure/MyDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure
+{
+    public class MyDbContext
+    {
+        public DbSet<User> Blogs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+    }
+}
