8dbe635d397babda086dde6dbf232c0a0fb8c9ed
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-20 19:22:49 +0100

    Fix build and refactor

--- src/WebApi/Infrastructure/MyDbContext.cs
+++ src/WebApi/Infrastructure/MyDbContext.cs
@@ -1,10 +1,19 @@
-﻿namespace Infrastructure
+﻿using Application;
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
 {
-    public class MyDbContext
+    public class MyDbContext : DbContext
     {
+        public MyDbContext(IConfigurationProvider configurationProvider)
+        {
+
+        }
+
         public DbSet<User> Blogs { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=blogging.db");
+            => options.UseSqlServer("Data Source=blogging.db");
     }
 }
