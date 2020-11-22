345e893e4bcf56636d9f7a343cc0c3a6b9f387ea
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-22 19:28:16 +0100

    Add usecase and presenter

---
+++ src/WebApi/Application/UseCases/GetUser/GetUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.GetUser
+{
+    public class GetUser : IGetUser
+    {
+        private IOutputPort _outputPort;
+
+        public GetUser()
+        {
+            this._outputPort = new GetAccountPresenter();
+        }
+        public Task Execute(GetUserInput input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOutputPort(IOutputPort outputPort)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task GetUserInternal(AccountId accountId)
+        {
+
+            this._outputPort.NotFound();
+        }
+    }
+}
