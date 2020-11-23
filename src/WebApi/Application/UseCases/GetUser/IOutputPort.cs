2fad237fae61fdd3d8dea214b0923c50f311f336
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-11-23 19:29:55 +0100

    Update output port

--- src/WebApi/Application/UseCases/GetUser/IOutputPort.cs
+++ src/WebApi/Application/UseCases/GetUser/IOutputPort.cs
@@ -1,6 +1,23 @@
 ﻿namespace Application.UseCases.GetUser
 {
+    /// <summary>
+    ///     Output Port.
+    /// </summary>
     public interface IOutputPort
     {
+        /// <summary>
+        ///     Invalid input.
+        /// </summary>
+        void Invalid();
+
+        /// <summary>
+        ///     Account closed.
+        /// </summary>
+        void NotFound();
+
+        /// <summary>
+        ///     Account closed.
+        /// </summary>
+        void Ok(UserModel account);
     }
 }
