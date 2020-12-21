f046c8b0c144aa4e444e64aed8e8520660cfacd3
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-12-21 18:37:22 +0100

    Fix test

--- src/WebApi/IntegrationTests/UnitTest1.cs
+++ src/WebApi/IntegrationTests/UnitTest1.cs
@@ -24,9 +24,7 @@ namespace IntegrationTests
             var response = await client.GetAsync(url);
 
             // Assert
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("text/html; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
+            response.EnsureSuccessStatusCode();
         }
     }
 }
