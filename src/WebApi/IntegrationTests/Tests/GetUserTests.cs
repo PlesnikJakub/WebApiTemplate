ec699f19a5b1f23f19d84fcb5b2dcf7de92a2ba2
Jakub Plesnik <jakub.plesnik@sde.cz>
2020-12-22 09:58:35 +0100

    Update int tests structure

--- src/WebApi/IntegrationTests/Tests/GetUserTests.cs
+++ src/WebApi/IntegrationTests/Tests/GetUserTests.cs
@@ -1,23 +1,26 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace IntegrationTests
+namespace IntegrationTests.Tests
 {
-    public class UnitTest1 : IClassFixture<WebApplicationFactory<WebApi.Startup>>
+    public class GetUserTests : IClassFixture<WebApplicationFactory<WebApi.Startup>>
     {
+        private readonly string _url = "/api/v1/Users/";
         private readonly WebApplicationFactory<WebApi.Startup> _factory;
 
-        public UnitTest1(WebApplicationFactory<WebApi.Startup> factory)
+        public GetUserTests(WebApplicationFactory<WebApi.Startup> factory)
         {
             _factory = factory;
         }
 
-        [Theory]
-        [InlineData("/api/v1/Users/1f26b2d6-42a6-4c58-a23a-3c00dd54f349")]
-        public async Task Test1(string url)
+        [Fact]
+        public async Task GetUser_ExistingUser_ShouldReturnUser()
         {
             // Arrange
+            var userGuid = Guid.Parse("1f26b2d6-42a6-4c58-a23a-3c00dd54f349");
+            var url = _url + userGuid;
             var client = _factory.CreateClient();
 
             // Act
