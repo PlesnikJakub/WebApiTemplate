d4ed7bd22c2dc4168d147b35ca8c137ec6cf4062
Jakub Plesnik <jakub.plesnik@sde.cz>
2021-05-08 09:44:44 +0200

    Add anotation

--- src/WebApi/WebApi/UseCases/v1/CreateUser/CreateUserRequest.cs
+++ src/WebApi/WebApi/UseCases/v1/CreateUser/CreateUserRequest.cs
@@ -1,11 +1,18 @@
-﻿namespace WebApi.UseCases.v1.CreateUser
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.UseCases.v1.CreateUser
 {
     public record CreateUserRequest
     {
+        [Required]
+        [MaxLength(250)]
         public string Name { get; init; }
-        
+
+        [Required]
+        [MaxLength(250)]
         public string Email { get; init; }
 
+        [Required]
         public string Age { get; init; }
     }
 }
--- src/WebApi/WebApi/UseCases/v1/GetUser/GetUserResponse.cs
+++ src/WebApi/WebApi/UseCases/v1/GetUser/GetUserResponse.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.UseCases.v1.GetUser
 {
     public record GetUserResponse
     {
+        [Required]
         public Guid Guid { get; init; }
     }
 }
