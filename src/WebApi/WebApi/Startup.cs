f57acaaf9689fc6197ba8770f16ede61f82ecf11
Jakub Plesnik <jakub.plesnik@3pillarglobal.com>
2022-01-14 13:24:39 +0100

    refactor: use file scope namespace

--- src/WebApi/WebApi/Startup.cs
+++ src/WebApi/WebApi/Startup.cs
@@ -6,55 +6,54 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebApi.Modules;
 
-namespace WebApi
+namespace WebApi;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddHealthChecks();
+
+        services.AddVersioning();
+
+        services.AddSwaggerGen();
+
+        services.AddUseCases();
+
+        services.AddPersistence(Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseDeveloperExceptionPage();
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseHttpsRedirection();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseVersionedSwagger(provider, this.Configuration, env);
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddControllers();
+            endpoints.MapControllers();
 
-            services.AddHealthChecks();
-
-            services.AddVersioning();
-
-            services.AddSwaggerGen();
-
-            services.AddUseCases();
-
-            services.AddPersistence(Configuration);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseVersionedSwagger(provider, this.Configuration, env);
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-
-                endpoints.MapHealthChecks("/health");
-            });
-        }
+            endpoints.MapHealthChecks("/health");
+        });
     }
 }
